378d1fb9b73b7923834745bd9a80c50ff1153273
luislodos-atlantico <luislodos@dpi.atlanticomedio.es>
2021-01-25 09:47:01 +0000

    Update

---
+++ Scripts/CambioColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CambioColor : MonoBehaviour
+{
+    public Color nuevoColor = Color.red;
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            sprite.color = nuevoColor;
+        }
+    }
+}
