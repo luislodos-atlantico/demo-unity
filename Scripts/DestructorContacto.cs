23e591adfa2ad8b64c113378a031be1f969da2fa
luislodos-atlantico <luislodos@dpi.atlanticomedio.es>
2021-01-25 09:39:20 +0000

    Update

--- Scripts/DestructorContacto.cs
+++ Scripts/DestructorContacto.cs
@@ -3,7 +3,7 @@
 public class DestructorContacto : MonoBehaviour
 {
     public GameObject explosion;
-
+   
     void OnCollisionEnter2D(Collision2D collision)
     {
         Instantiate(explosion, transform.position, transform.rotation);
---
+++ Scripts/Movimiento/CambioPosicion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioPosicion : MonoBehaviour
+{
+    public float velocidad = 1;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            var x = transform.position.x + velocidad * Time.deltaTime;
+            var y = transform.position.y;
+            transform.position = new Vector2(x, y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            var x = transform.position.x - velocidad * Time.deltaTime;
+            var y = transform.position.y;
+            transform.position = new Vector2(x, y);
+        }
+    }
+}
