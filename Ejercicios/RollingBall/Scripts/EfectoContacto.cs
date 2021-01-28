3882edd774dbb2898eaa9d143102bd580338414e
luislodos-atlantico <luislodos@dpi.atlanticomedio.es>
2021-01-28 09:18:23 +0000

    Update

--- Ejercicios/RollingBall/Scripts/EfectoContacto.cs
+++ Ejercicios/RollingBall/Scripts/EfectoContacto.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class EfectoContacto : MonoBehaviour
 {
     public ParticleSystem particulas;
+    AudioSource fuenteSonido;
+    Animator animador;
 
     void Start()
     {
         particulas.Stop();
+        fuenteSonido = GetComponent<AudioSource>();
+        animador = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         particulas.Play();
+        fuenteSonido.Play();
+        animador.Play("CambioColor");
     }
 }
