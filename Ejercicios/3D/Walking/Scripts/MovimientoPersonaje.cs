013c7cf4501ee1c1b95f82365f10314adf248d53
luislodos-atlantico <luislodos@dpi.atlanticomedio.es>
2021-03-23 08:38:11 +0000

    Update

---
+++ Ejercicios/3D/Walking/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 playerVelocity;
+    bool groundedPlayer;
+    float playerSpeed = 2.0f;
+    float jumpHeight = 1.0f;
+    float gravityValue = -9.81f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
